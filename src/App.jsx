a00401c610730b87fd0cfd033c344e19c1874683
kamser <keylor.morataya@gmail.com>
2026-04-20 22:50:40 -0700

    Replaced the clear function for the removeItem for LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -60,8 +60,8 @@ function App() {
     setTurn(TURNS.X);
     setBoard(Array(9).fill(null));
     setWinner(null);
-    localStorage.clear('gameBoard');
-    localStorage.clear('currentTurn');
+    localStorage.removeItem('gameBoard');
+    localStorage.removeItem('currentTurn');
   }
 
   return (
